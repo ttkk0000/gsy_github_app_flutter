285472a16636c8334a925501e594864e691ed540
Dean Tang <ttkk000@163.com>
2023-03-07 16:22:20 +0800

    注释无用代码

--- lib/app.dart
+++ lib/app.dart
@@ -45,12 +45,12 @@ class _FlutterReduxAppState extends State<FlutterReduxApp>
         locale: Locale('zh', 'CH')),
   );
 
-  ColorFilter greyscale = ColorFilter.matrix(<double>[
-    0.2126, 0.7152, 0.0722, 0, 0,
-    0.2126, 0.7152, 0.0722, 0, 0,
-    0.2126, 0.7152, 0.0722, 0, 0,
-    0,      0,      0,      1, 0,
-  ]);
+  // ColorFilter greyscale = ColorFilter.matrix(<double>[
+  //   0.2126, 0.7152, 0.0722, 0, 0,
+  //   0.2126, 0.7152, 0.0722, 0, 0,
+  //   0.2126, 0.7152, 0.0722, 0, 0,
+  //   0,      0,      0,      1, 0,
+  // ]);
 
 
   @override
